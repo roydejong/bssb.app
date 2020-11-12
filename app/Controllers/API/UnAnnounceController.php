6e6550de0485e161bd4aaff43f3cfd54b90fc37c
Roy de Jong <roy@softwarepunt.nl>
2020-11-12 16:45:40 +0100

    UnAnnounce controller: basic implementation

--- app/Controllers/API/UnAnnounceController.php
+++ app/Controllers/API/UnAnnounceController.php
@@ -3,11 +3,35 @@
 namespace app\Controllers\API;
 
 use app\HTTP\Request;
+use app\HTTP\Responses\JsonResponse;
+use app\Models\HostedGame;
 
 class UnAnnounceController
 {
     public function unAnnounce(Request $request)
     {
-        die("hi {$request->method} {$request->path}");
+        // -------------------------------------------------------------------------------------------------------------
+        // Input
+
+        $serverCode = $request->queryParams['serverCode'] ?? null; // unused right now because what's the point
+        $ownerId = $request->queryParams['ownerId'] ?? null;
+
+        // -------------------------------------------------------------------------------------------------------------
+        // Execute
+
+        $anyDeletes = false;
+
+        if ($ownerId) {
+            if (HostedGame::query()->delete()->where("owner_id = ?", $ownerId)->execute() > 0) {
+                $anyDeletes = true;
+            }
+        }
+
+        // -------------------------------------------------------------------------------------------------------------
+        // Response
+
+        return new JsonResponse([
+            "result" => $anyDeletes ? "deleted" : "noop",
+        ]);
     }
 }
