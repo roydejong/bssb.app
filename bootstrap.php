9c252d62be51291d320ece2245637ed7c3abb589
Roy de Jong <roy@softwarepunt.nl>
2020-11-16 22:51:05 +0100

    GH Actions: Attempt to enable tests w/ MySQL service (5/?)

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-use Instasell\Instarecord\Instarecord;
-
 define('DIR_BASE', __DIR__);
 define('DIR_VIEWS', DIR_BASE . "/views");
 define('DIR_STORAGE', DIR_BASE . "/storage");
@@ -9,7 +7,3 @@ define('DIR_CACHE', DIR_STORAGE . "/cache");
 
 require_once DIR_BASE . "/vendor/autoload.php";
 require_once "config.php";
-
-$dbConfig = Instarecord::config();
-
-echo "Target test database: {$dbConfig->database} ({$dbConfig->username}@{$dbConfig->host})" . PHP_EOL;
--- phpunit.php
+++ phpunit.php
@@ -1,3 +1,19 @@
 <?php
 
+use Instasell\Instarecord\Instarecord;
+
 require_once __DIR__ . "/bootstrap.php";
+
+$dbConfig = Instarecord::config();
+
+$ENV_DB_PORT = intval(getenv('DB_PORT'));
+
+if ($ENV_DB_PORT > 0) {
+    $dbConfig->port = $ENV_DB_PORT;
+}
+
+if ($dbConfig->unix_socket) {
+    echo "Target test database: {$dbConfig->database} ({$dbConfig->username}@{$dbConfig->unix_socket})" . PHP_EOL;
+} else {
+    echo "Target test database: {$dbConfig->database} ({$dbConfig->username}@{$dbConfig->host}:{$dbConfig->port})" . PHP_EOL;
+}
