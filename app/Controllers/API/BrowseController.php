72f14f519f553d9d0afbf725b0bf8243f5e63175
Roy de Jong <roy@softwarepunt.nl>
2020-11-28 17:31:56 +0100

    Game sort order: Somewhat prefer lonely games

--- app/Controllers/API/BrowseController.php
+++ app/Controllers/API/BrowseController.php
@@ -61,7 +61,7 @@ class BrowseController
             ->where("last_update >= ?", HostedGame::getStaleGameCutoff())
             ->andWhere("ended_at IS NULL")
             ->leftJoin("level_records lr ON (lr.level_id = hosted_games.level_id)")
-            ->orderBy("player_count >= player_limit ASC, player_limit DESC, hosted_games.id DESC");
+            ->orderBy("player_count >= player_limit ASC, player_count > 1 ASC, player_limit DESC, hosted_games.id DESC");
 
         // Search query
         if (!empty($searchQuery)) {
