bd43e6194c4f0410a228e819739c93ce78bb128c
Roy de Jong <roy@softwarepunt.nl>
2021-01-16 22:24:45 +0100

    feat(stats): Add Top 100 sub pages

--- app/Controllers/StatsController.php
+++ app/Controllers/StatsController.php
@@ -5,17 +5,44 @@ namespace app\Controllers;
 use app\Frontend\ResponseCache;
 use app\Frontend\View;
 use app\HTTP\Request;
+use app\HTTP\Responses\NotFoundResponse;
 use app\Models\HostedGame;
 use app\Models\LevelRecord;
 
 class StatsController
 {
-    const CACHE_KEY = "stats_page";
-    const CACHE_TTL = 60;
+    // -----------------------------------------------------------------------------------------------------------------
+    // Consts
+
+    const CACHE_KEY_MAIN = "stats_page";
+    const CACHE_TTL_MAIN = 60;
+
+    const CACHE_KEY_TOP_LEVELS_PREFIX = "stats_top_levels_";
+    const CACHE_TTL_TOP_LEVELS = 120;
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Consts
+
+    private function queryTopLevels(bool $customs = true, int $offset = 0, int $pageSize = 10): array
+    {
+        $query = LevelRecord::query()
+            ->where('stat_play_count > 0')
+            ->offset($offset)
+            ->limit(10)
+            ->orderBy('stat_play_count DESC');
+
+        if ($customs) {
+            $query->andWhere('hash IS NOT NULL');
+        } else {
+            $query->andWhere('hash IS NULL');
+        }
+
+        return $query->queryAllModels();
+    }
 
     public function getStats(Request $request)
     {
-        $resCache = new ResponseCache(self::CACHE_KEY, self::CACHE_TTL);
+        $resCache = new ResponseCache(self::CACHE_KEY_MAIN, self::CACHE_TTL_MAIN);
 
         if ($resCache->getIsAvailable()) {
             return $resCache->readAsResponse();
@@ -33,17 +60,8 @@ class StatsController
             ->select("SUM(stat_play_count) AS count")
             ->querySingleValue());
 
-        $topLevelsCustom = LevelRecord::query()
-            ->limit(10)
-            ->orderBy('stat_play_count DESC')
-            ->where('hash IS NOT NULL')
-            ->queryAllModels();
-
-        $topLevelsOfficial = LevelRecord::query()
-            ->limit(10)
-            ->orderBy('stat_play_count DESC')
-            ->where('hash IS NULL')
-            ->queryAllModels();
+        $topLevelsCustom = $this->queryTopLevels(true, 0, 10);
+        $topLevelsOfficial = $this->queryTopLevels(false, 0, 10);
 
         $view = new View('stats.twig');
         $view->set('pageUrl', '/stats');
@@ -54,6 +72,40 @@ class StatsController
             'topLevelsCustom' => $topLevelsCustom,
             'topLevelsOfficial' => $topLevelsOfficial
         ]);
+
+        $response = $view->asResponse();
+        $resCache->writeResponse($response);
+        return $response;
+    }
+
+    public function getTopLevelsSubPage(Request $request, string $urlSection)
+    {
+        switch ($urlSection) {
+            case "custom-levels":
+                $showCustomLevels = true;
+                $pageTitle = "Top 100 Custom Levels";
+                break;
+            case "official-levels":
+                $showCustomLevels = false;
+                $pageTitle = "Top 100 Official Levels";
+                break;
+            default:
+                return new NotFoundResponse();
+        }
+
+        $resCacheKey = self::CACHE_KEY_TOP_LEVELS_PREFIX . $urlSection;
+        $resCache = new ResponseCache($resCacheKey, self::CACHE_TTL_TOP_LEVELS);
+
+        if ($resCache->getIsAvailable()) {
+            return $resCache->readAsResponse();
+        }
+
+        $topLevels = $this->queryTopLevels($showCustomLevels, 0, 100);
+
+        $view = new View('stats_top_levels.twig');
+        $view->set('pageUrl', "/stats/top/{$urlSection}");
+        $view->set('pageTitle', $pageTitle);
+        $view->set('levels', $topLevels);
 
         $response = $view->asResponse();
         $resCache->writeResponse($response);
--- public/index.php
+++ public/index.php
@@ -18,6 +18,7 @@ $router = new RequestRouter();
 // Site routes
 $router->register('/', [new HomeController(), 'index']);
 $router->register('/stats', [new StatsController(), 'getStats']);
+$router->register('/stats/top/$urlSection', [new StatsController(), 'getTopLevelsSubPage']);
 $router->register('/game/$hashId', [new GameDetailController(), 'getGameDetail']);
 $router->register('/privacy', [new PrivacyController(), 'getPrivacy']);
 
