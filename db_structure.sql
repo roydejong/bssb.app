b115b3c872db2a49539bb38e530f0952f77561f4
Roy de Jong <roy@softwarepunt.nl>
2021-01-25 02:01:52 +0100

    maint(db): remove create db / use statement from sql file
    the use statement is breaking GH actions so yeah

--- db_structure.sql
+++ db_structure.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `bssb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `bssb`;
 -- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
 --
 -- Host: wsl.local    Database: bssb
