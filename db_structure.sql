5236ebc9b2d8b27c77338a0849d30ed8c36db595
Roy de Jong <roy@softwarepunt.nl>
2022-01-17 00:04:09 +0100

    maint: update db_structure.sql  (added characteristic)

--- db_structure.sql
+++ db_structure.sql
@@ -1,150 +1,124 @@
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
---
--- Host: wsl.local    Database: bssb_test
--- ------------------------------------------------------
--- Server version	8.0.25-15
+/*
+ Navicat MySQL Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+ Source Server         : Local WSL
+ Source Server Type    : MySQL
+ Source Server Version : 80027
+ Source Host           : ubuntu.wsl:3306
+ Source Schema         : bssb
 
---
--- Table structure for table `hosted_game_announcers`
---
+ Target Server Type    : MySQL
+ Target Server Version : 80027
+ File Encoding         : 65001
 
+ Date: 17/01/2022 00:03:34
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for hosted_game_announcers
+-- ----------------------------
 DROP TABLE IF EXISTS `hosted_game_announcers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `hosted_game_announcers` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `player_id` int unsigned NOT NULL,
-  `game_id` int unsigned NOT NULL,
+CREATE TABLE `hosted_game_announcers`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `player_id` int UNSIGNED NOT NULL,
+  `game_id` int UNSIGNED NOT NULL,
   `last_announce` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `id_UNIQUE`(`id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
---
--- Table structure for table `hosted_game_players`
---
-
+-- ----------------------------
+-- Table structure for hosted_game_players
+-- ----------------------------
 DROP TABLE IF EXISTS `hosted_game_players`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `hosted_game_players` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `hosted_game_id` int unsigned NOT NULL,
+CREATE TABLE `hosted_game_players`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `hosted_game_id` int UNSIGNED NOT NULL,
   `sort_index` int NOT NULL,
-  `user_id` varchar(255) NOT NULL,
-  `user_name` varchar(255) NOT NULL,
-  `is_host` tinyint NOT NULL DEFAULT '0',
-  `is_announcer` tinyint NOT NULL DEFAULT '0',
-  `latency` decimal(8,4) NOT NULL DEFAULT '0.0000',
-  `is_connected` tinyint NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `hgp_game_and_sort_index` (`hosted_game_id`,`sort_index`),
-  KEY `hgp_game_id_idx` (`hosted_game_id`),
+  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `is_host` tinyint NOT NULL DEFAULT 0,
+  `is_announcer` tinyint NOT NULL DEFAULT 0,
+  `latency` decimal(8, 4) NOT NULL DEFAULT 0.0000,
+  `is_connected` tinyint NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `id_UNIQUE`(`id` ASC) USING BTREE,
+  UNIQUE INDEX `hgp_game_and_sort_index`(`hosted_game_id` ASC, `sort_index` ASC) USING BTREE,
+  INDEX `hgp_game_id_idx`(`hosted_game_id` ASC) USING BTREE,
   CONSTRAINT `hgp_game_id` FOREIGN KEY (`hosted_game_id`) REFERENCES `hosted_games` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=261944 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE = InnoDB AUTO_INCREMENT = 262238 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
---
--- Table structure for table `hosted_games`
---
-
+-- ----------------------------
+-- Table structure for hosted_games
+-- ----------------------------
 DROP TABLE IF EXISTS `hosted_games`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `hosted_games` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `server_code` varchar(5) NOT NULL,
-  `game_name` varchar(255) NOT NULL,
-  `owner_id` varchar(255) NOT NULL,
-  `owner_name` varchar(255) NOT NULL,
-  `player_count` tinyint unsigned NOT NULL DEFAULT '0',
-  `player_limit` tinyint unsigned NOT NULL DEFAULT '0',
-  `is_modded` tinyint unsigned NOT NULL DEFAULT '0',
+CREATE TABLE `hosted_games`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `server_code` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `game_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `owner_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `owner_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `player_count` tinyint UNSIGNED NOT NULL DEFAULT 0,
+  `player_limit` tinyint UNSIGNED NOT NULL DEFAULT 0,
+  `is_modded` tinyint UNSIGNED NOT NULL DEFAULT 0,
   `first_seen` datetime NOT NULL,
   `last_update` datetime NOT NULL,
-  `lobby_state` tinyint unsigned NOT NULL,
-  `level_id` varchar(255) DEFAULT NULL,
-  `song_name` varchar(255) DEFAULT NULL,
-  `song_author` varchar(255) DEFAULT NULL,
-  `difficulty` tinyint unsigned DEFAULT NULL,
-  `platform` varchar(16) NOT NULL DEFAULT 'unknown',
-  `master_server_host` varchar(255) DEFAULT NULL,
-  `master_server_port` int unsigned DEFAULT NULL,
-  `ended_at` datetime DEFAULT NULL,
-  `mp_ex_version` varchar(45) DEFAULT NULL,
-  `mod_name` varchar(45) NOT NULL DEFAULT 'ServerBrowser',
-  `mod_version` varchar(16) DEFAULT NULL,
-  `game_version` varchar(16) DEFAULT NULL,
-  `server_type` varchar(32) DEFAULT 'player_host',
-  `host_secret` varchar(32) DEFAULT NULL,
-  `endpoint` varchar(255) DEFAULT NULL,
-  `manager_id` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=66171 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `lobby_state` tinyint UNSIGNED NOT NULL,
+  `level_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `song_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `song_author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `difficulty` tinyint UNSIGNED NULL DEFAULT NULL,
+  `characteristic` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `platform` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'unknown',
+  `master_server_host` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `master_server_port` int UNSIGNED NULL DEFAULT NULL,
+  `ended_at` datetime NULL DEFAULT NULL,
+  `mp_ex_version` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `mod_name` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'ServerBrowser',
+  `mod_version` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `game_version` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `server_type` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT 'player_host',
+  `host_secret` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `endpoint` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `manager_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 67350 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
---
--- Table structure for table `level_records`
---
-
+-- ----------------------------
+-- Table structure for level_records
+-- ----------------------------
 DROP TABLE IF EXISTS `level_records`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `level_records` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `level_id` varchar(255) NOT NULL,
-  `hash` varchar(128) DEFAULT NULL,
-  `beatsaver_id` varchar(128) DEFAULT NULL,
-  `cover_url` varchar(255) DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `song_name` varchar(255) NOT NULL,
-  `song_author` varchar(255) DEFAULT NULL,
-  `level_author` varchar(255) DEFAULT NULL,
-  `duration` int DEFAULT NULL,
-  `description` text,
-  `stat_play_count` int unsigned DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `level_id_UNIQUE` (`level_id`),
-  UNIQUE KEY `beatsaver_id_UNIQUE` (`beatsaver_id`),
-  UNIQUE KEY `hash_UNIQUE` (`hash`)
-) ENGINE=InnoDB AUTO_INCREMENT=45813 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `level_records`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `level_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `hash` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `beatsaver_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `cover_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `song_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `song_author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `level_author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `duration` int NULL DEFAULT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `stat_play_count` int UNSIGNED NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `level_id_UNIQUE`(`level_id` ASC) USING BTREE,
+  UNIQUE INDEX `beatsaver_id_UNIQUE`(`beatsaver_id` ASC) USING BTREE,
+  UNIQUE INDEX `hash_UNIQUE`(`hash` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 45896 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
---
--- Table structure for table `system_config`
---
-
+-- ----------------------------
+-- Table structure for system_config
+-- ----------------------------
 DROP TABLE IF EXISTS `system_config`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `system_config` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `server_message` text,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+CREATE TABLE `system_config`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `server_message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `id_UNIQUE`(`id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-09-15 22:08:20
+SET FOREIGN_KEY_CHECKS = 1;
