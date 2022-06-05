445077d2cfae9035dd66e777c2774187b1a2a023
Roy de Jong <roy@softwarepunt.nl>
2022-06-06 01:27:16 +0200

    maint(ci): debugging mysql error during migrations

--- phinx.php
+++ phinx.php
@@ -6,6 +6,10 @@ require_once "bootstrap.php";
 
 $dbConfig = Instarecord::config();
 
+if (($ENV_DB_PORT = intval(getenv('DB_PORT'))) > 0) {
+    $dbConfig->port = $ENV_DB_PORT;
+}
+
 return [
     'environments' => [
         'default_environment' => 'default',
