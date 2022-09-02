9f84d32b68dbe3b2042965ad96bf84c72d0afb76
Roy de Jong <roy@softwarepunt.nl>
2022-09-02 17:30:48 +0200

    feat(GeoIp): back with Redis cache / lazy loading

--- app/External/GeoIp.php
+++ app/External/GeoIp.php
@@ -2,24 +2,42 @@
 
 namespace app\External;
 
+use app\BSSB;
 use MaxMind\Db\Reader;
+use function Sentry\captureException;
 
 require_once __DIR__ . "/geoip2/geoip2.phar";
 
 final class GeoIp
 {
-    private static ?GeoIp $instance = null;
+    private const CacheTtlSeconds = 86400;
+
+    private bool $dbLoaded;
     private ?Reader $reader;
+
+    private static ?GeoIp $instance;
 
     public function __construct()
     {
+        $this->dbLoaded = false;
+        $this->reader = null;
+
+        self::$instance = $this;
+    }
+
+    private function lazyLoad(): void
+    {
+        if ($this->dbLoaded)
+            return;
+
         try {
             $this->reader = new Reader(__DIR__ . '/geoip2/GeoLite2-City.mmdb');
-        } catch (\Exception) {
+        } catch (\Exception $ex) {
+            captureException($ex);
             $this->reader = null;
         }
 
-        self::$instance = $this;
+        $this->dbLoaded = true;
     }
 
     public static function instance(): GeoIp
@@ -27,31 +45,65 @@ final class GeoIp
         return self::$instance ?? new GeoIp();
     }
 
-    public function lookupRecord(string $ipOrEndpoint): ?array
+    private function lookupRecord(string $ipOrEndpoint): ?array
     {
-        if (!$this->reader) {
-            return null;
-        }
-
+        // Remove port if this is an endpoint string
         if (str_contains($ipOrEndpoint, ':')) {
-            // Remove port if this is an endpoint string
             $parts = explode(':', $ipOrEndpoint, 2);
             $ip = $parts[0];
         } else {
             $ip = $ipOrEndpoint;
         }
 
+        // Check for cached record
+        $redis = BSSB::getRedis();
+
+        $ipHash = md5($ip);
+        $hashKey = "geoip:{$ipHash}";
+
+        if ($cachedRecord = $redis->getArrayHash($hashKey))
+            return $cachedRecord;
+
+        // Load database if needed
+        $this->lazyLoad();
+
+        if (!$this->reader)
+            return null;
+
+        // Try get record, writing to cache if valid
         try {
-            return $this->reader->get($ip);
+            $recordReduced = self::reduceRecord($this->reader->get($ip));
+            $redis->setArrayHash($hashKey, $recordReduced, self::CacheTtlSeconds);
+            return $recordReduced;
         } catch (\Exception) {
             return null;
         }
     }
 
+    private static function reduceRecord(?array $record): array
+    {
+        $reduced = [
+            'valid' => $record !== null,
+            'country_iso' => $record['country']['iso_code'] ?? null,
+            'country_name' => $record['country']['names']['en'] ?? null,
+            'subdivisions_text' => null
+        ];
+
+        if (isset($record['subdivisions'])) {
+            $parts = [];
+            foreach ($record['subdivisions'] as $subdivision)
+                $parts[] = $subdivision['names']['en'];
+            if (!empty($parts))
+                $reduced['subdivisions_text'] = implode(', ', $parts);
+        }
+
+        return $reduced;
+    }
+
     public function getCountryCode(string $ipOrEndpoint): ?string
     {
         if ($record = $this->lookupRecord($ipOrEndpoint)) {
-            return $record['country']['iso_code'];
+            return $record['country_iso'];
         }
         return null;
     }
@@ -61,20 +113,15 @@ final class GeoIp
         if ($record = $this->lookupRecord($ipOrEndpoint)) {
             $parts = [];
 
-            // Subdivision (state)
-            if ($includeSubdivisons && isset($record['subdivisions'])) {
-                foreach ($record['subdivisions'] as $subdivision) {
-                    $parts[] = $subdivision['names']['en'];
-                }
-            }
+            if ($includeSubdivisons && !empty($record['subdivisions_text']))
+                $parts[] = $record['subdivisions_text'];
 
-            // Country name
-            if ($includeCountry) {
-                $parts[] = $record['country']['names']['en'];
-            }
+            if ($includeCountry && !empty($record['country_name']))
+                $parts[] = $record['country_name'];
 
             return implode(', ', $parts);
         }
+
         return null;
     }
 
