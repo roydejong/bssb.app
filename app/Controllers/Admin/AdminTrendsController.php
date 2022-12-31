e4da6d1a541d4e9a290d2535977f18bbbf50c735
Roy de Jong <roy@softwarepunt.nl>
2022-12-31 22:14:32 +0100

    feat(admin/trends): filters, "all" trends filter

--- app/Controllers/Admin/AdminTrendsController.php
+++ app/Controllers/Admin/AdminTrendsController.php
@@ -5,22 +5,59 @@ namespace app\Controllers\Admin;
 use app\Frontend\View;
 use app\HTTP\Request;
 use app\HTTP\Response;
+use app\HTTP\Responses\RedirectResponse;
 use app\Models\LevelRecord;
 
 class AdminTrendsController extends BaseAdminController
 {
     public function getTrends(Request $request): Response
     {
-        $levels = LevelRecord::query()
-            ->where('hash IS NOT NULL')
-            ->limit(100)
-            ->orderBy('trend_factor DESC')
-            ->queryAllModels();
+        // -------------------------------------------------------------------------------------------------------------
+        // Filter switch
+
+        $filterOptions = [
+            "trends" => "Trending by score (customs)",
+            "trendsall" => "Trending by score (all)"
+        ];
+
+        $queryFilter = $request->queryParams['filter'] ?? "trends";
+
+        if (!isset($filterOptions[$queryFilter]))
+            // Invalid filter in query
+            return new RedirectResponse('/admin/trends');
+
+        $filterName = $filterOptions[$queryFilter];
+
+        // -------------------------------------------------------------------------------------------------------------
+        // Query
+
+        $levelsQuery = LevelRecord::query()
+            ->limit(100);
+
+        switch ($queryFilter) {
+            default:
+            case "trends":
+                $levelsQuery
+                    ->andWhere('hash IS NOT NULL')
+                    ->orderBy('trend_factor DESC');
+                break;
+            case "trendsall":
+                $levelsQuery
+                    ->orderBy('trend_factor DESC');
+                break;
+        }
+
+        $levels = $levelsQuery->queryAllModels();
+
+        // -------------------------------------------------------------------------------------------------------------
+        // Present
 
         $view = new View('admin/trends.twig');
-        $view->set('title', 'Trends');
+        $view->set('title', $filterName);
         $view->set('tab', 'trends');
         $view->set('levels', $levels);
+        $view->set('filters', $filterOptions);
+        $view->set('selectedFilterId', $queryFilter);
         return $view->asResponse();
     }
 }
