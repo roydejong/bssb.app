be6c7482ebf07d0107387dc854dca9057af5628d
Roy de Jong <roy@softwarepunt.nl>
2024-01-10 18:20:29 +0100

    tweak: limit profile calc job to 50 per run

--- tasks/UpdatePlayerProfileStatsTask.php
+++ tasks/UpdatePlayerProfileStatsTask.php
@@ -10,10 +10,15 @@ $task = $schedule->run(function () {
     require_once __DIR__ . "/../bootstrap.php";
 
     $pendingStats = ProfileStats::queryAllPending();
+    $limit = 50; // Limit to 50 updates per run/minute
 
     foreach ($pendingStats as $profileStats) {
         if ($player = Player::fetch($profileStats->playerId)) {
             $profileStats->recalculate($player);
+            $limit--;
+        }
+        if ($limit <= 0) {
+            break;
         }
     }
 });
