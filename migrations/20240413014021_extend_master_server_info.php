95aa87e8a65a2363d682f2b3be257982d938666f
Roy de Jong <roy@softwarepunt.nl>
2024-04-13 04:14:50 +0200

    tweak: store name, description, image, max players for master servers

---
+++ migrations/20240413014021_extend_master_server_info.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+final class ExtendMasterServerInfo extends AbstractMigration
+{
+    public function change(): void
+    {
+        $this->table('master_server_info')
+            ->addColumn('description', 'text', ['after' => 'nice_name', 'null' => true, 'default' => null, 'length' => MysqlAdapter::TEXT_MEDIUM])
+            ->addColumn('image_url', 'string', ['after' => 'description', 'null' => true, 'default' => null])
+            ->addColumn('max_players', 'integer', ['after' => 'image_url', 'null' => true, 'default' => null])
+            ->update();
+    }
+}
